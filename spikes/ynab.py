fe1c9af6c831f5fec048ae808f4922357bb307d7
FeliPython <fmorais@consumeraffairs.com>
2021-09-07 22:53:43 -0300

    format file

--- spikes/ynab.py
+++ spikes/ynab.py
@@ -6,7 +6,7 @@ headers = {'Authorization': 'Bearer ' + auth_token,
            'Content-Type': 'application/json'}
 
 nuconta_account_id = "d928b335-f1c5-4cf4-8d8b-99d6d8df67bb"
-credit_card_accoun_id = "e98ed2b0-2c5e-4f37-b82d-9e1fb5caa510"
+credit_card_account_id = "e98ed2b0-2c5e-4f37-b82d-9e1fb5caa510"
 data = {
     "transaction": {
         "account_id": nuconta_account_id,
