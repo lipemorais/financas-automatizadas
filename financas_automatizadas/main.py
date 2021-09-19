31937d2fe479c792e249995ab650e6c4ed88470b
FeliPython <fmorais@consumeraffairs.com>
2021-09-19 20:40:50 -0300

    extract accont_sync and report

--- financas_automatizadas/main.py
+++ financas_automatizadas/main.py
@@ -8,6 +8,20 @@ from financas_automatizadas import ynab
 
 
 def main(nubank_client: Nubank = Nubank()) -> [dict]:
+    created_account_transactions_in_ynab = sync_account(nubank_client)
+
+    report_transactions(created_account_transactions_in_ynab)
+
+    return created_account_transactions_in_ynab
+
+
+def report_transactions(created_transaction_in_ynab):
+    pprint("REPORT")
+    pprint("Account transactions: ")
+    pprint(f"{created_transaction_in_ynab}")
+
+
+def sync_account(nubank_client):
     account_feed = nubank_client.get_account_feed()
 
     filtered_account_transactions = nubank.filter_account_transactions(
@@ -17,9 +31,5 @@ def main(nubank_client: Nubank = Nubank()) -> [dict]:
     created_transaction_in_ynab = ynab.send_account_transaction(
         account_transactions=filtered_account_transactions
     )
-
-    pprint("\n\n\n REPORT \n\n\n")
-    pprint("Created transaction: ")
-    pprint(f"{created_transaction_in_ynab}")
 
     return created_transaction_in_ynab
