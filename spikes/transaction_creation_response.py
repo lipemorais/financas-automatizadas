f9aabfa7cfe1f6f94ac3caa10b04daa253be5a70
FeliPython <fmorais@consumeraffairs.com>
2021-09-19 22:12:35 -0300

    add response example from ynab

---
+++ spikes/transaction_creation_response.py
@@ -0,0 +1,27 @@
+r = {
+    "data": {
+        "transaction_ids": ["e2852594-8fcd-48af-ba13-7df857cdc142"],
+        "transaction": {
+            "id": "e2852594-8fcd-48af-ba13-7df857cdc142",
+            "date": "2021-08-26",
+            "amount": 18839,
+            "memo": "Bourbon Ipiranga - supermercado",
+            "cleared": "cleared",
+            "approved": True,
+            "flag_color": None,
+            "account_id": "e98ed2b0-2c5e-4f37-b82d-9e1fb5caa510",
+            "account_name": "Nubank",
+            "payee_id": None,
+            "payee_name": None,
+            "category_id": None,
+            "category_name": "Uncategorized",
+            "transfer_account_id": None,
+            "transfer_transaction_id": None,
+            "matched_transaction_id": None,
+            "import_id": "61281a14-2674-45d6-8e50-1c097bdc966f",
+            "deleted": False,
+            "subtransactions": [],
+        },
+        "server_knowledge": 9406,
+    }
+}
