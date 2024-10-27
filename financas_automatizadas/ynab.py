932001dd6347a0887bd4580d899962ea34654b78
FeliPython <felipejpa15@gmail.com>
2024-10-27 19:25:06 -0300

    Refactor transaction sending functions
    Consolidate the functionality of sending account and card transactions into a single `send_transactions_to_ynab` function. This includes introducing helper functions to determine transaction amounts and refactoring the payload construction for YNAB API requests.

--- financas_automatizadas/ynab.py
+++ financas_automatizadas/ynab.py
@@ -1,74 +1,44 @@
 import requests
 from decouple import config
 
+from schemas import TransactionKind, Transaction
+
 auth_token = config("YNAB_TOKEN")
 headers = {"Authorization": "Bearer " + auth_token, "Content-Type": "application/json"}
 
-nuconta_account_id = "d928b335-f1c5-4cf4-8d8b-99d6d8df67bb"
-credit_card_account_id = "e98ed2b0-2c5e-4f37-b82d-9e1fb5caa510"
-INFLOW = ("nuds_v2_icon.money_in",)
 
 
-OUTFLOW = (
-    "nuds_v2_icon.bar_code",
-    "nuds_v2_icon.card",
-    "nuds_v2_icon.money_out",
-)
+def get_amount(transaction: Transaction) -> int:
+    if transaction.kind == TransactionKind.DEBIT:
+        return -abs(transaction.amount)
+    else:  # TransactionKind is CREDIT
+        return +abs(transaction.amount)
 
 
-def send_account_transaction(account_transactions: [dict]) -> [dict]:
+def send_transactions_to_ynab(transactions: [Transaction], account_id) -> [dict]:
     base_url = "https://api.youneedabudget.com/v1"
     moraix_budget_id = "72bf90ed-5c22-4f88-bc02-95fcd82474cb"
     url = f"{base_url}/budgets/{moraix_budget_id}/transactions"
     created_transactions = []
 
-    for account_transaction in account_transactions:
-        amount = int(account_transaction["amount"] * 1000)
+    for transaction in transactions:
+        amount = get_amount(transaction)
+
         payload = {
             "transaction": {
-                "account_id": nuconta_account_id,
-                "date": account_transaction["postDate"],
-                "amount": amount
-                if account_transaction["iconKey"] in INFLOW
-                else -amount,
+                "account_id": account_id,
+                "date": transaction.date.strftime("%Y-%m-%d"),
+                "amount": amount,
                 "payee_id": None,
                 "payee_name": None,
                 "category_id": None,
-                "memo": account_transaction["detail"],
+                "memo": transaction.description,
                 "cleared": "cleared",
                 "approved": True,
-                "import_id": account_transaction["id"],
+                "import_id": transaction.external_id,
             }
         }
-
         response = requests.post(url, json=payload, headers=headers)
         created_transactions.append(response.json())
 
     return created_transactions
-
-
-def send_card_transaction(card_transactions: [dict]) -> [dict]:
-    base_url = "https://api.youneedabudget.com/v1"
-    moraix_budget_id = "72bf90ed-5c22-4f88-bc02-95fcd82474cb"
-    url = f"{base_url}/budgets/{moraix_budget_id}/transactions"
-    transactions = []
-
-    for card_transaction in card_transactions:
-        payload = {
-            "transaction": {
-                "account_id": credit_card_account_id,
-                "date": card_transaction["time"].split("T")[0],
-                "amount": -card_transaction["amount"] * 10,
-                "payee_id": None,
-                "payee_name": None,
-                "category_id": None,
-                "memo": f"{card_transaction['description']} - {card_transaction['title']}",
-                "cleared": "cleared",
-                "approved": True,
-                "import_id": card_transaction["id"],
-            }
-        }
-        response = requests.post(url, json=payload, headers=headers)
-        transactions.append(response.json())
-
-    return transactions
