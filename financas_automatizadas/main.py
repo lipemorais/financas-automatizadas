4b046757518173c8d3595a82b83d9d32e297ab9e
FeliPython <felipejpa15@gmail.com>
2024-10-27 19:28:52 -0300

    Refactor main.py to integrate with Pluggy for transactions
    Replaced Nubank client authentication with Pluggy transaction retrieval. Updated transaction syncing to handle multiple accounts by mapping Pluggy account IDs to YNAB account IDs.

--- financas_automatizadas/main.py
+++ financas_automatizadas/main.py
@@ -1,64 +1,63 @@
-from datetime import datetime, timedelta
-from pprint import pprint
+from decouple import config
 
-from dateutil.tz import tzutc
-from pynubank import Nubank
-
-from financas_automatizadas import nubank
 from financas_automatizadas import ynab
-from financas_automatizadas.nubank import setup_nubank_client_authentication
+from financas_automatizadas.my_pluggy import get_transactions
+from financas_automatizadas.schemas import Transaction
+from my_pluggy import get_api_key
 
 
-def main(nubank_client: Nubank = Nubank()) -> [dict]:
-    setup_nubank_client_authentication(nubank_client=nubank_client)
-    # created_account_transactions_in_ynab = sync_account(nubank_client)
-    created_card_transactions_in_ynab = sync_card(nubank_client)
-
-    # report_transactions("Nu Conta", created_account_transactions_in_ynab)
-    report_transactions("Cartão de crédito", created_card_transactions_in_ynab)
-
-    # return created_account_transactions_in_ynab + created_card_transactions_in_ynab
-    return created_card_transactions_in_ynab
-
-
-def report_transactions(account, created_transaction_in_ynab):
-    pprint("REPORT")
-    pprint(f"{account} transactions: ")
-    pprint(f"{created_transaction_in_ynab}")
-
-
-def sync_account(nubank_client: Nubank) -> [dict]:
-    pprint("Sync Account starting")
-    account_statements = nubank_client.get_account_statements_paginated()
-    # parse account_statements
-    filtered_account_transactions = nubank.filter_account_transactions(
-        account_statements=account_statements
+def main() -> [dict]:
+    PLUGGY_CLIENT_ID = config("PLUGGY_CLIENT_ID")
+    PLUGGY_CLIENT_SECRET = config("PLUGGY_CLIENT_SECRET")
+    api_key = get_api_key(
+        client_id=PLUGGY_CLIENT_ID, client_secret=PLUGGY_CLIENT_SECRET
     )
 
-    created_transactions_in_ynab = ynab.send_account_transaction(
-        account_transactions=filtered_account_transactions
-    )
+    # YNAB
+    ynab_nuconta_account_id = "d928b335-f1c5-4cf4-8d8b-99d6d8df67bb"
+    ynab_credit_card_account_id = "e98ed2b0-2c5e-4f37-b82d-9e1fb5caa510"
+    ynab_itau_account_id = "ad709b27-5540-4d2a-ab6a-66f0f4f0e18c"
 
-    pprint("Sync Account ending")
-    return created_transactions_in_ynab
+    # My pluggy
+    pluggy_itau_account_id = "7209f910-6108-4371-bbd2-e4eb2a678cc7"
+    pluggy_nubank_credit_card_id = "36b6b61c-0b07-419d-b9ec-cf3b4ea11d75"
+    pluggy_nubank_account_id = "b27ac45a-2e39-4581-989f-77d7c24ee8c4"
 
+    account_id_pairs = [
+        {
+            "pluggy": pluggy_itau_account_id,
+            "ynab": ynab_itau_account_id,
+            "name": "Itaú - Account",
+        },
+        {
+            "pluggy": pluggy_nubank_credit_card_id,
+            "ynab": ynab_credit_card_account_id,
+            "name": "Nubank - Credit Card",
+        },
+        {
+            "pluggy": pluggy_nubank_account_id,
+            "ynab": ynab_nuconta_account_id,
+            "name": "Nubank - Account",
+        },
+    ]
 
-def sync_card(nubank_client: Nubank) -> [dict]:
-    pprint("Sync Card starting")
-    card_statements = nubank_client.get_card_statements()
+    transactions = {}
+    for account_id_pair in account_id_pairs:
+        transactions: [Transaction] = get_transactions(
+            account_id=account_id_pair["pluggy"],
+            api_key=api_key,
+        )
 
-    yesterday = datetime.now(tz=tzutc()) - timedelta(days=1)
-    filtered_card_transactions = nubank.filter_card_transactions(
-        card_statements=card_statements, threshold=yesterday
-    )
+        transactions.append(
+            ynab.send_transactions_to_ynab(
+                transactions=transactions,
+                account_id=account_id_pair["ynab"],
+            )
+        )
 
-    created_transactions_in_ynab = ynab.send_card_transaction(
-        card_transactions=filtered_card_transactions
-    )
+    return transactions
 
-    pprint("Sync Card ending")
-    return created_transactions_in_ynab
 
 
 if __name__ == "__main__":
-    main(nubank_client=Nubank())
+    main()
