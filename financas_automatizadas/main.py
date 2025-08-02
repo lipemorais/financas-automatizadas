743783db99257fa8327704b18835b4953041c73d
FeliPython <felipejpa15@gmail.com>
2025-08-01 21:37:21 -0300

    Refactor YNAB sync process to use dynamic `get_account_pairs` function and correctly handle `YNAB_BUDGET_ID`

--- financas_automatizadas/main.py
+++ financas_automatizadas/main.py
@@ -1,6 +1,7 @@
 from decouple import config
 
 import ynab
+from config import get_account_pairs
 from my_pluggy import get_transactions
 from schemas import Transaction
 from my_pluggy import get_api_key
@@ -13,36 +14,10 @@ def main() -> [dict]:
         client_id=PLUGGY_CLIENT_ID,
         client_secret=PLUGGY_CLIENT_SECRET,
     )
+    ynab_budget_id = config("YNAB_BUDGET_ID")
+    account_id_pairs = get_account_pairs()
 
-    # YNAB
-    ynab_nuconta_account_id = "d928b335-f1c5-4cf4-8d8b-99d6d8df67bb"
-    ynab_credit_card_account_id = "e98ed2b0-2c5e-4f37-b82d-9e1fb5caa510"
-    ynab_itau_account_id = "ad709b27-5540-4d2a-ab6a-66f0f4f0e18c"
-
-    # My pluggy
-    pluggy_itau_account_id = "7209f910-6108-4371-bbd2-e4eb2a678cc7"
-    pluggy_nubank_credit_card_id = "36b6b61c-0b07-419d-b9ec-cf3b4ea11d75"
-    pluggy_nubank_account_id = "b27ac45a-2e39-4581-989f-77d7c24ee8c4"
-
-    account_id_pairs = [
-        {
-            "pluggy": pluggy_itau_account_id,
-            "ynab": ynab_itau_account_id,
-            "name": "Itaú - Account",
-        },
-        {
-            "pluggy": pluggy_nubank_credit_card_id,
-            "ynab": ynab_credit_card_account_id,
-            "name": "Nubank - Credit Card",
-        },
-        {
-            "pluggy": pluggy_nubank_account_id,
-            "ynab": ynab_nuconta_account_id,
-            "name": "Nubank - Account",
-        },
-    ]
-
-    transactions = {}
+    created_transactions = []
     for account_id_pair in account_id_pairs:
         print("######")
         print(f'SYNCING {account_id_pair["name"]}')
@@ -53,17 +28,18 @@ def main() -> [dict]:
             api_key=api_key,
         )
 
-        transactions.append(
-            ynab.send_transactions_to_ynab(
-                transactions=transactions,
-                account_id=account_id_pair["ynab"],
-            )
+        transactions_to_ynab = ynab.send_transactions_to_ynab(
+            transactions=transactions,
+            account_id=account_id_pair["ynab"],
+            budget_id=ynab_budget_id,
         )
 
+        created_transactions += transactions_to_ynab
+
     print("######")
-    print(f"SYNCED {len(transactions)} TRANSACTIONS")
+    print(f"SYNCED {len(created_transactions)} TRANSACTIONS")
     print("######")
-    return transactions
+    return created_transactions
 
 
 
